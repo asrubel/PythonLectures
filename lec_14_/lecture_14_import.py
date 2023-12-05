2bec220a655ee1fafcd6d78d53f25b5f3a2f0375
Oleksii <oleksii.rubel@nixs.com>
2023-12-05 11:53:12 +0100

    Lecture 14 - fixing

--- lec_14_/lecture_14_import.py
+++ lec_14_/lecture_14_import.py
@@ -1,4 +1,4 @@
-# import lecture_14_modules
+import lecture_14_modules
 from lecture_14_modules import a, MyClass, my_fun
 import package1.module1
 import package1.module2
@@ -7,16 +7,16 @@ from package2.module2 import Class2
 
 
 print(__name__)
-# print(lecture_14_modules.MyClass())
-# print(lecture_14_modules.my_fun(2, 8))
-# print(lecture_14_modules.a)
+print(lecture_14_modules.MyClass())
+print(lecture_14_modules.my_fun(2, 8))
+print(lecture_14_modules.a)
 
-# print(a)
-# print(MyClass())
-# print(my_fun(10, 5))
+print(a)
+print(MyClass())
+print(my_fun(10, 5))
 
-# print(package1.module1.my_fun(3))
-# print(package1.module2.my_fun(3))
+print(package1.module1.my_fun(3))
+print(package1.module2.my_fun(3))
 
 print(Class1())
 print(Class2())
