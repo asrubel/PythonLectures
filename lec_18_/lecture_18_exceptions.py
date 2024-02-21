45ecdedbf541b9fc31bb358010c97f953a03042f
Oleksii <oleksii.rubel@nixs.com>
2024-02-21 23:44:14 +0100

    Lecture 18 Exceptions

---
+++ lec_18_/lecture_18_exceptions.py
@@ -0,0 +1,104 @@
+"""Exceptions"""
+
+print("Python lecture")
+
+
+def my_fun():
+    """My function"""
+    for i in range(5):
+        print(i)
+
+
+my_fun()
+
+print("Calculate numbers...")
+a = 5
+b = 0
+print("Error!")
+print(a / b)
+print("Unreachable code")
+
+# BaseException
+#  +-- SystemExit
+#  +-- KeyboardInterrupt
+#  +-- GeneratorExit
+#  +-- BaseExceptionGroup
+#  +-- Exception
+#       +-- StopIteration
+#       +-- StopAsyncIteration
+#       +-- ArithmeticError
+#       |    +-- FloatingPointError
+#       |    +-- OverflowError
+#       |    +-- ZeroDivisionError
+#       +-- AssertionError
+#       +-- AttributeError
+#       +-- BufferError
+#       +-- EOFError
+#       +-- ExceptionGroup [BaseExceptionGroup]
+#       +-- ImportError
+#       |    +-- ModuleNotFoundError
+#       +-- LookupError
+#       |    +-- IndexError
+#       |    +-- KeyError
+#       +-- MemoryError
+#       +-- NameError
+#       |    +-- UnboundLocalError
+#       +-- OSError
+#       |    +-- BlockingIOError
+#       |    +-- ChildProcessError
+#       |    +-- ConnectionError
+#       |    |    +-- BrokenPipeError
+#       |    |    +-- ConnectionAbortedError
+#       |    |    +-- ConnectionRefusedError
+#       |    |    +-- ConnectionResetError
+#       |    +-- FileExistsError
+#       |    +-- FileNotFoundError
+#       |    +-- InterruptedError
+#       |    +-- IsADirectoryError
+#       |    +-- NotADirectoryError
+#       |    +-- PermissionError
+#       |    +-- ProcessLookupError
+#       |    +-- TimeoutError
+#       +-- ReferenceError
+#       +-- RuntimeError
+#       |    +-- NotImplementedError
+#       |    +-- RecursionError
+#       +-- SyntaxError
+#       |    +-- IndentationError
+#       |         +-- TabError
+#       +-- SystemError
+#       +-- TypeError
+#       +-- ValueError
+#       |    +-- UnicodeError
+#       |         +-- UnicodeDecodeError
+#       |         +-- UnicodeEncodeError
+#       |         +-- UnicodeTranslateError
+#       +-- Warning
+#            +-- DeprecationWarning
+#            +-- EncodingWarning
+#            +-- PendingDeprecationWarning
+#            +-- RuntimeWarning
+#            +-- SyntaxWarning
+#            +-- UserWarning
+#            +-- FutureWarning
+#            +-- ImportWarning
+#            +-- UnicodeWarning
+#            +-- BytesWarning
+#            +-- ResourceWarning
+
+print("Calculate numbers...")
+
+try:
+    a = int(input("Please, enter the first number: "))
+    b = int(input("Please, enter the second number: "))
+    c = a / b
+    print("Something...")
+except ZeroDivisionError:
+# except ArithmeticError:
+    print("Please enter non-zero second number!")
+except (ValueError, TypeError):
+    print("Wrong types or values!")
+else:
+    print(c)
+finally:
+    print("All done!")
