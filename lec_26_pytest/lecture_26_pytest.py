d8ae6882bb24dc2816d33c3bf3bb9c9afe1fd0ab
Oleksii <oleksii.rubel@nixs.com>
2024-04-09 14:37:57 +0200

    Lecture 26 Pytest

---
+++ lec_26_pytest/lecture_26_pytest.py
@@ -0,0 +1,85 @@
+import pytest
+
+
+def test_uppercase():
+    assert "python".upper() == "PYTHON"
+
+
+def test_reversed():
+    assert list(reversed([1, 2, 3, 4])) == [4, 3, 2, 1]
+
+
+@pytest.fixture
+def example_fixture():
+    return 1
+
+
+def test_with_fixture(example_fixture):
+    assert example_fixture == 1
+
+
+@pytest.fixture
+def example_people_data():
+    return [
+        {
+            "given_name": "Alfonsa",
+            "family_name": "Ruiz",
+            "title": "Senior Software Engineer",
+        },
+        {
+            "given_name": "Sayid",
+            "family_name": "Khan",
+            "title": "Project Manager",
+        },
+    ]
+
+
+def format_data_for_display(people):
+    return [f"{p['given_name']} {p['family_name']}: {p['title']}"
+            for p in people]
+
+
+def format_data_for_excel(people):
+    return ""
+
+
+def test_format_data_for_display(example_people_data):
+    assert format_data_for_display(example_people_data) == [
+        "Alfonsa Ruiz: Senior Software Engineer",
+        "Sayid Khan: Project Manager",
+    ]
+
+
+def test_format_data_for_excel(example_people_data):
+    assert format_data_for_excel(example_people_data) == """given,family,title
+Alfonsa,Ruiz,Senior Software Engineer
+Sayid,Khan,Project Manager
+"""
+
+
+def is_palindrome(data):
+    return True
+
+
+@pytest.mark.parametrize("palindrome", [
+    "",
+    "a",
+    "Bob",
+    "Never odd or even",
+    "Do geese see God?",
+])
+def test_is_palindrome(palindrome):
+    assert is_palindrome(palindrome)
+
+
+@pytest.mark.parametrize("maybe_palindrome, expected_result", [
+    ("", True),
+    ("a", True),
+    ("Bob", True),
+    ("Never odd or even", True),
+    ("Do geese see God?", True),
+    ("abc", False),
+    ("abab", False),
+])
+def test_is_palindrome(maybe_palindrome, expected_result):
+    assert is_palindrome(maybe_palindrome) == expected_result
